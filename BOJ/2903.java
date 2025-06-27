f0da3187ea8d579061f12813561dcafabe4e2792
gyanni <lghlee36@gmail.com>
2025-06-27 10:31:38 +0900

    test

---
+++ BOJ/2903.java
@@ -0,0 +1,5 @@
+public class 2903 {
+    public static void main(String argc[]) {
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
